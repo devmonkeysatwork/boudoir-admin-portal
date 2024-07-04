5046b41240fa4f315b914370445124f3c178e3f2
touseef <touseefktk22@gmail.com>
2024-07-04 22:22:10 +0500

    barcode testing page added

---
+++ resources/views/barcode-scanner.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="container mt-5">
+                    <h2>Barcode Scanner</h2>
+                    <input type="text" id="barcode_input" class="form-control" placeholder="Scan barcode here">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="barcodeModal" tabindex="-1" aria-labelledby="barcodeModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="barcodeModalLabel">Scanned Barcode</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p id="barcode_result"></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Include jQuery and Bootstrap JS for modal functionality -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#barcode_input').on('keypress', function(event) {
+                if (event.keyCode === 13) { // Enter key pressed
+                    var barcode = $(this).val();
+                    $('#barcode_result').text(barcode);
+                    $('#barcodeModal').modal('show');
+                    $(this).val(''); // Clear the input field
+                }
+            });
+        });
+    </script>
+@endsection
