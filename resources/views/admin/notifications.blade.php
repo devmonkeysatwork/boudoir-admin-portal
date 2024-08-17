65a645399aa6b014e13f1e04ca48f1b3edda3f33
touseef <touseefktk22@gmail.com>
2024-08-18 01:41:46 +0500

    redirection from notification

--- resources/views/admin/notifications.blade.php
+++ resources/views/admin/notifications.blade.php
@@ -11,9 +11,17 @@
                   <p class="p12 position-relative fw-normal">
                       @if($notification->type == \App\Models\Notifications::typeComment)
                            {{$notification->comment->user->name}} added a comment on order id <span class="fw-bold">{{$notification->comment->order->order_id}}</span>
+                          @php
+                            $order_id = $notification->comment->order->order_id
+                          @endphp
                       @else
-                          {{$notification->log?->user?->name}} updated a status for order id <span class="fw-bold">{{$notification->log?->status?->status_name}}</span>
+                          {{$notification->log?->user?->name}} updated a status for order id <span class="fw-bold">{{$notification->log?->order_id}}</span>
+                          @php
+                              $order_id = $notification->log?->order_id
+                          @endphp
                       @endif
+                          <br>
+                          <a href="{{route('admin.orders')}}?order_id={{$order_id}}&tab=open">View Details</a>
                           <span class="notification_time">{{$notification->created_at}}</span>
                   </p>
               </td>
