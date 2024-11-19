213412b78542c6a85c60257cd5c0d22e02271fee
touseef <touseefktk22@gmail.com>
2024-11-19 21:38:54 +0500

    fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -358,7 +358,7 @@
         // Verify that the scanned QR matches the order ID
         if (qrInput !== orderNumber) {
             alert('Scanned QR code does not match the order number.');
-            startScanning();
+            $('.btn-rescan-order').fadeIn();
             return;
         }
         let data  = new FormData();
